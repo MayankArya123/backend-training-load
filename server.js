c67627a9a76c38befe1fe46467df69ec3563803d
mayank arya <mayanksunny786@gmail.com>
2026-01-19 13:14:25 +0530

    frontend-url-added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // your Next.js frontend
+    origin: "https://frontend-training-load.vercel.app", // your Next.js frontend
     credentials: true,
   }),
 );
